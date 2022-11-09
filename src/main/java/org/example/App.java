0fa00c54b0b2433237145feb69bd2fa43d21ffef
Auguest <715484127@qq.com>
2022-11-10 00:48:55 +0800

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.reader.StreamReader;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
 /**
- * Hello world!
+ * Kafka Demo
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        InputStream input = null;
+        input = StreamReader.class.getClassLoader().getResourceAsStream("application.yml");
+        Yaml yaml = new Yaml();
+        Map<String, Object> object = yaml.load(input);
+        object.get("kafka");
     }
 }
